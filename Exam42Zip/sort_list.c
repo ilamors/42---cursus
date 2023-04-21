0a43b27367553549949127a9d9ff501c30d8ca08
ilamors <laila.morsli@proton.me>
2023-04-21 23:46:47 +0200

    Add files via upload
    Signed-off-by: ilamors <laila.morsli@proton.me>

---
+++ Exam42Zip/sort_list.c
@@ -0,0 +1,72 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
+{
+	int	swap;
+	t_list	*tmp;
+
+	tmp = lst;
+	while(lst->next != NULL)
+	{
+		if (((*cmp)(lst->data, lst->next->data)) == 0)
+		{
+			swap = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = swap;
+			lst = tmp;
+		}
+		else
+			lst = lst->next;
+	}
+	lst = tmp;
+	return (lst);
+}
+
+
+void	swap_values(t_list *a, t_list *b);
+t_list	*sort_list(t_list *lst, int (*cmp)(int, int));
+
+int ascending(int a, int b)
+{
+	return (a <= b);
+}
+
+int	main(void)
+{
+	t_list *c = malloc(sizeof(t_list));
+	c->next = 0;
+	c->data = 45;
+
+	t_list *b = malloc(sizeof(t_list));
+	b->next = c;
+	b->data = 73;
+
+	t_list *a = malloc(sizeof(t_list));
+	a->next = b;
+	a->data = 108;
+
+	t_list *cur = a;
+	while (cur)
+	{
+		printf("%d", cur->data);
+		if (cur->next != 0)
+			printf(", ");
+		cur = cur->next;
+	}
+	printf("\n");
+
+	cur = sort_list(a, ascending);
+
+	// cur = a;
+	while (cur)
+	{
+		printf("%d", cur->data);
+		if (cur->next != 0)
+			printf(", ");
+		cur = cur->next;
+	}
+	printf("\n");
+}
+
