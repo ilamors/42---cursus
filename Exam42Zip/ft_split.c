9b6ec8b9ea5b2c512ca0762d6fa7ab0980301572
ilamors <laila.morsli@proton.me>
2023-05-24 23:57:11 +0200

    Update ft_split.c
    Signed-off-by: ilamors <laila.morsli@proton.me>

--- Exam42Zip/ft_split.c
+++ Exam42Zip/ft_split.c
@@ -1,80 +1,83 @@
 #include <stdlib.h>
 
+int	len_word(char *str)
+{
+	int	i = 0;
+	while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+		++i;
+	return(i);
+}
+
+char	*word_dupe(char *str)
+{
+	int	len = len_word(str);
+	int	i = 0;
+	char	*tab = malloc(sizeof(char) * (len + 1));
+	if (!tab)
+		return(NULL);
+	tab[len] = '\0';
+	while (i < len)
+	{
+		tab[i] = str[i];
+		++i;
+	}
+	return(tab);
+}
+
+void	fill_word(char **tab, char *str)
+{
+	int	i = 0;
+	while (*str == ' ' || *str == '\t' || *str == '\n')
+		++str;
+	while (*str)
+	{
+		tab[i] = word_dupe(str);
+			++i;
+		while (*str && *str != ' ' && *str != '\t' && *str != '\n')
+			++str;
+		while (*str == ' ' || *str == '\t' || *str == '\n')
+			++str;
+	}
+}
+
 int	count_word(char *str)
 {
-	int	count;
-
-	count = 0;
-	while(*str)
+	int count = 0;
+	while (*str == ' ' || *str == '\t' || *str == '\n')
+		++str;
+	while (*str)
 	{
-		while(*str && (*str == ' ' || *str == '\t' || *str == '\n'))
-			str++;
-		if(*str && *str != ' ' && *str != '\t' && *str != '\n')
-		{
-			count++;
-			while (*str && *str != ' ' && *str != '\t' && *str != '\n')
-				str++;
-
-		}
+		++count;
+		while (*str && *str != ' ' && *str != '\t' && *str != '\n')
+			++str;
+		while (*str == ' ' || *str == '\t' || *str == '\n')
+			++str;
 	}
 	return(count);
 }
 
-char	*malloc_word(char *str)
-{
-	int	i;
-	char	*tab;
-
-	i = 0;
-	while (str[i] && str[i] != ' ' && str[i] != '\n' && str[i] != '\t')
-		i++;
-	tab = malloc(sizeof(char) * i + 1);
-	if (!tab)
-		return(NULL);
-	i = 0;
-	while (str[i] && str[i] != ' ' && str[i] != '\n' && str[i] != '\t')
-	{
-		tab[i] = str[i];
-		i++;
-	}
-	tab[i] = '\0';
-	return(tab);
-}
-
 char	**ft_split(char *str)
 {
-	int	i;
-	char	**dest;
-
-	i = 0;
-	dest = malloc(sizeof(char*) * count_word(str) + 1);
-	if(!dest)
+	if (str == NULL)
 		return(NULL);
-	while(*str)
-	{
-		while(*str && (*str == ' ' || *str == '\t' || *str == '\n'))
-                        str++;
-                if(*str && *str != ' ' && *str != '\t' && *str != '\n')
-                {
-			dest[i] = malloc_word(str);
-			i++;
-			while (*str && *str != ' ' && *str != '\t' && *str != '\n')
-                                str++;
-		}
-	}
-	dest[i] = '\0';
-	return(dest);
+	int	i = count_word(str);
+	char	**tab = malloc(sizeof(char*) * (i + 1));
+	if (!tab)
+		return(NULL);
+	tab[i] = '\0';
+	fill_word(tab, str);
+	return(tab);
 }
 
 #include <stdio.h>
 int	main()
 {
 	int	i = 0;
-	char	*s = "le petit chien aboie lorsque la caravane passe";
+	char	*s = "Ce clavier gestionnaire permet aux clients de personnaliser leur ordinateur sur un clavier niveau.";
 	char	**str = ft_split(s);
-	while(str[i])
+	while (str[i])
 	{
 		printf("%s\n", str[i]);
-		i++;
+			i++;
 	}
 }
